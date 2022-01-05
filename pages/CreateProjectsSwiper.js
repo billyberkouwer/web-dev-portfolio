d646ba8191b33d9dfed40a500bb5153de335a953
billyberkouwer <billyberkouwer@live.com>
2022-01-05 12:26:52 +0000

    swiper

--- pages/CreateProjectsSwiper.js
+++ pages/CreateProjectsSwiper.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { Autoplay, Pagination, EffectCube } from 'swiper';
+import { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -14,21 +14,20 @@ const CreateProjectsSwiper = (props) => {
     
     if (i === 0) {
         projectSlides.push(
-            images.map(image => <SwiperSlide key={image}><Image alt={image} src={image} layout="fill" priority={true} objectFit="cover" /></SwiperSlide>)
+            images.map(image => <SwiperSlide key={image}><Image quality={50} alt={image} src={image} layout="fill" priority={true} objectFit="cover" /></SwiperSlide>)
         )
     }   else {
         projectSlides.push(
-            images.map(image => <SwiperSlide key={image}><Image alt={image} src={image} layout="fill" objectFit="cover" /></SwiperSlide>)
+            images.map(image => <SwiperSlide key={image}><Image quality={50} alt={image} src={image} layout="fill" objectFit="cover" /></SwiperSlide>)
         )
     }
 
 
     return (
         <Swiper
-        style={{ '--swiper-theme-color': 'darkgreen', '--swiper-navigation-size' : '30px' }}
         modules={[Autoplay]}
         className={styles.swiperContainer}
-        autoplay={{delay: 7000}}
+        autoplay={{delay: 5000}}
         spaceBetween={50}
         loop
         speed={1000}
